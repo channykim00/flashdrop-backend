19dc512269ba807459d2872d26ca42ba5ae39100
channykim00 <hadeskim0429@gmail.com>
2025-07-03 01:31:20 +0900

    feat: 디바이스 온라인 상태 확인 API 및 소켓 디바이스 매핑 기능 추가

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import { Server as SocketIOServer } from "socket.io";
 
 import linkRoutes from "./routes/links/index.js";
 import receiveRoutes from "./routes/receive/index.js";
+import { deviceSocketMap } from "./socket/socketStore.js";
 
 dotenv.config();
 
@@ -27,8 +28,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/links", linkRoutes);
 app.use("/api/receive", receiveRoutes);
-
-const deviceSocketMap = new Map();
 
 io.on("connection", (socket) => {
   console.log("소켓 연결됨:", socket.id);
--- routes/receive/getLink.js
+++ routes/receive/getLink.js
@@ -15,6 +15,7 @@ router.get("/:uniqueUrl", async (req, res) => {
 
     const filteredLink = {
       title: link.title,
+      deviceId: link.deviceId,
       expireTime: link.expireTime,
       allowedFileTypes: link.allowedFileTypes,
       maxFileSize: link.maxFileSize,
