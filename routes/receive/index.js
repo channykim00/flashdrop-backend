391769cb78111c632ca47ee80fa05c8b2c157cac
channykim00 <hadeskim0429@gmail.com>
2025-07-16 22:30:03 +0900

    refactor: 필요 없는 isDeviceOnline 함수 제거 및 라우트 제거

--- routes/receive/index.js
+++ routes/receive/index.js
@@ -1,13 +1,11 @@
 import express from "express";
 
 import getLink from "./getLink.js";
-import isOnline from "./isOnline.js";
 import verifyPassword from "./verifyPassword.js";
 
 const router = express.Router();
 
 router.use("/", getLink);
 router.use("/", verifyPassword);
-router.use("/", isOnline);
 
 export default router;
--- utils/isDeviceOnline.js
+++
@@ -1,8 +0,0 @@
-import { deviceSocketMap } from "../socket/socketStore.js";
-
-const isDeviceOnline = (deviceId) => {
-  if (!deviceId) return false;
-  return deviceSocketMap.has(deviceId);
-};
-
-export default isDeviceOnline;
